a4456630551de3254372f9731e562a066359b947
Sandu Guzun <sanduguzun762@gmail.com>
2025-05-14 20:32:34 +0300

    helo

--- src/main/java/com/pixelacademy/springdemo_app/rest/HelloWorldRestControler.java
+++ src/main/java/com/pixelacademy/springdemo_app/rest/HelloWorldRestControler.java
@@ -1,7 +1,13 @@
 package com.pixelacademy.springdemo_app.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldRestControler {
 
-
-
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
 }
